3816b0c266bdb4d98301736ffd46dadc29f4785d
sahil kapoor <sahilkapoor440@gmail.com>
2020-01-17 12:07:27 +0530

    initialised

---
+++ gatsby-config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-blog`,
+      options: {},
+    },
+  ],
+  // Customize your site metadata:
+  siteMetadata: {
+    title: `Sahil's Blog`,
+    author: `Sahil Kapoor`,
+    description: `Journey of an average fullstack developer`,
+    social: [
+      {
+        name: `twitter`,
+        url: `https://twitter.com/gatsbyjs`,
+      },
+      {
+        name: `github`,
+        url: `https://github.com/gatsbyjs`,
+      },
+    ],
+  },
+}
